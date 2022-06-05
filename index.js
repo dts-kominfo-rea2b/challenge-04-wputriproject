3e3da32f6b802a381fdf443c2157fb35662d2d05
Putri Project <95266772+wputriproject@users.noreply.github.com>
2022-06-05 21:33:51 +0700

    finished

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+    "2019-01-02", // 1546387200 (epoch time, dalam detik)
+    "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
+    "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
+    "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
+    "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (date, num) => {
+    const cDate = [];
+    date.forEach((e) => {
+        const tDate = (new Date(e).getTime() / 1000).toString();
+
+        cDate.push(tDate);
+    });
+
+    if (num === undefined) {
+        return cDate.sort((a, b) => a - b).join("-");
+    } else {
+        const sortDate = cDate.sort((a, b) => a + b);
+        return sortDate[num];
+    }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
-  // IIFE
+    // IIFE
 
-  // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
-  console.log(createDate?.(dates));
+    // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
+    console.log(createDate?.(dates));
 
-  // '1614841200' (dalam string)
-  console.log(createDate?.(dates, 2));
+    // '1614841200' (dalam string)
+    console.log(createDate?.(dates, 2));
 })();
 
 module.exports = {
-  dates,
-  createDate,
+    dates,
+    createDate,
 };
